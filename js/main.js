5a1269557fa59c41bed879b0719f58709371c3b1
Robert Perry <30665739+robertrpjr@users.noreply.github.com>
2017-11-30 22:04:53 -0600

    reworked call with jQuery

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
-$(document).ready(() => {
+/*var divContents = document.getElementById("results");
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.github.com/events');
 
-	//Make Github request
-    $.ajax({
-	url:'https://api.github.com/events',
-	dataType: "json",
-	success: function(results){
-		$('.results').html(returndata[0]["actor"]['display_login']);
+request.onload = function(){
+	var selectedData = JSON.parse(request.responseText);
+	for (i = 0; i < 25; i++){
+		
+	$('.results').html(`Username: ${selectedData[i].actor.login} <br> 
+		Type: ${selectedData[i].type}<br>
+		Repo Name: ${selectedData[i].repo.name}<br>`);
 	
-});
+	}
+	
+};
+		
+request.send();
+*/
+
+$.getJSON('https://api.github.com/events').done(data => {
+	$('.results').append(data.map(datum =>
+		`Username: ${datum.actor.login} <br> 
+		 Type: ${datum.type} <br>
+		 Repo Name: ${datum.repo.name}<br><br>`
+	))
+  })
