26e7b5d8fff6febd9c9d78ccf1fb6a048f1e9f37
Robert Perry <30665739+robertrpjr@users.noreply.github.com>
2017-11-30 22:07:45 -0600

    removes comments

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-/*var divContents = document.getElementById("results");
-var request = new XMLHttpRequest();
-request.open('GET', 'https://api.github.com/events');
-
-request.onload = function(){
-	var selectedData = JSON.parse(request.responseText);
-	for (i = 0; i < 25; i++){
-		
-	$('.results').html(`Username: ${selectedData[i].actor.login} <br> 
-		Type: ${selectedData[i].type}<br>
-		Repo Name: ${selectedData[i].repo.name}<br>`);
-	
-	}
-	
-};
-		
-request.send();
-*/
-
 $.getJSON('https://api.github.com/events').done(data => {
 	$('.results').append(data.map(datum =>
 		`Username: ${datum.actor.login} <br> 
