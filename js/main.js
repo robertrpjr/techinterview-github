507184e6f7addc5243078a8f7d23987656d8487e
Robert Perry <30665739+robertrpjr@users.noreply.github.com>
2017-12-04 17:41:17 -0600

    finished functionality
    functionality finished,
    updated css

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
 $(document).ready(function(){
-$.ajax({
+           $.ajax({
 		url: 'https://api.github.com/events',
 		data:{
 			client_id:'87d1ff436a9e7041f831',
 			client_secret:'7de9b627d5d12024c52b89cc9e52bf2e693ab13c'
 			}
-		}).done(data => {
+		}).done(function(data) {
 			$('#results').html(data.map(datum =>
-				`
+			        `
 				<button class="tapMe">
-					<div class="panel panel-default">
-						<div class="panel-heading">
-						  <h3 class="panel-title">Username: ${datum.actor.login}</h3>
-						</div>
-					<div class="col-md-3">
-						<img class="thumbnail" src="${datum.actor.avatar_url}"
-					</div>
-					<div class="col-md-9">
-						Type: ${datum.type} <br>
-						Repo Name: ${datum.repo.name}
-					</div>
-				  </div>
+				   <div class="panel panel-default">
+				      <div class="panel-heading">
+			                 <h3 class="panel-title">Username: ${datum.actor.login}</h3>
+			              </div>
+				      <div class="panel-body">
+				         <div class="row">
+				            <div class="col-xs-4"> 
+					       <img class="thumbnail" src="${datum.actor.avatar_url}">
+				            </div>
+                                            <div class="col-xs-8">
+					    <div class="details">
+					       Type: ${datum.type} <br>
+					       Repo Name: ${datum.repo.name}
+					    </div>
+				            <div class="data2 hidden">
+				    		<span>${JSON.stringify(datum)}</span>
+				    	    </div>
+					  </div>
+                                        </div>
+				      </div>
+				    </div>
 				</button>
-				`,
+				`,				
 				$('#results').on('click', function(){
-					console.log('${datum.payload}');
+                    		$('.data2').toggleClass('hidden');
+				$('.details').toggleClass('hidden');
 				})
 				))	
 			})
